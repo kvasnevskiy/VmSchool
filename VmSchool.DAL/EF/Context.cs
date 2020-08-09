b1962ddde63f0fa6674b1c2f65e4996560061319
Vlad <vkvasn@gmail.com>
2020-08-09 18:34:50 +0300

    Добавьте файлы проекта.

---
+++ VmSchool.DAL/EF/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VmSchool.DAL.EF
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=VmSchoolDB;Trusted_Connection=True;");
+        }
+    }
+}
